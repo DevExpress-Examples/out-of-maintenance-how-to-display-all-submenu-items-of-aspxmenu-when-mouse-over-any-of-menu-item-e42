2b9ca927672f9f0dc13145c8c8b25c281419a3ae
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to display all submenu items of ASPxMenu when mouse over any of menu item
+
+
+<p>An ASPxMenu does not allow this by default. But there is the following workaround. You can add ASPxPopupControl and prepare a dictionary of sitemap items. Then, bind this datasource (dictionary) to the Repeater placed on the ASPxPopupControl to display sub-menu items when the popup window is shown.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private nodes As New Dictionary(Of String, SiteMapNode)()
+
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim smNodeCollection As SiteMapNodeCollection = siteMap.Provider.RootNode.GetAllNodes()
+		For Each node As SiteMapNode In smNodeCollection
+			If node.ParentNode.Title = String.Empty Then
+				nodes.Add(node.Title, node)
+				menu.Items.Add(node.Title)
+			End If
+		Next node
+		columnsRpt.DataSource = nodes.Keys
+		columnsRpt.DataBind()
+	End Sub
+
+	Protected Sub columnsRpt_ItemDataBound(ByVal sender As Object, ByVal e As RepeaterItemEventArgs)
+		If e.Item.ItemType = ListItemType.Item OrElse e.Item.ItemType = ListItemType.AlternatingItem Then
+			Dim rpt As Repeater = CType(e.Item.FindControl("rowsRpt"), Repeater)
+			rpt.DataSource = nodes(e.Item.DataItem.ToString()).ChildNodes
+			rpt.DataBind()
+		End If
+	End Sub
+End Class
